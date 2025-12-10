a7d3010826020f90f981f611b0b0973ce6f99ae6
hanggaa <hanggaprihadi@gmail.com>
2025-12-10 11:38:03 +0700

    Updates

--- assets/animation-vendor-Cb0faKQx.js
+++ assets/animation-vendor-Cb0faKQx.js
@@ -1,4 +1,4 @@
-import{r as v}from"./react-vendor-BB5n2w1Y.js";var Ms={exports:{}},Ht={};/**
+import{r as v}from"./react-vendor-19brgfdU.js";var Ms={exports:{}},Ht={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
